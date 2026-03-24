91f949161a25537edccdd15652e6628d4b774dab
LenZz21 <lenzz21@users.noreply.github.com>
2026-03-24 12:32:47 +0800

    Revert "Fix Railway session path by ensuring storage dirs exist"
    This reverts commit c03323bfa17957cd6fb7cbc1076bb55de9c8cd54.

--- public/index.php
+++ public/index.php
@@ -5,22 +5,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Ensure runtime-writable storage directories exist in ephemeral environments (e.g. Railway).
-$requiredStorageDirectories = [
-    __DIR__.'/../storage/framework',
-    __DIR__.'/../storage/framework/cache',
-    __DIR__.'/../storage/framework/sessions',
-    __DIR__.'/../storage/framework/testing',
-    __DIR__.'/../storage/framework/views',
-    __DIR__.'/../storage/logs',
-];
-
-foreach ($requiredStorageDirectories as $directory) {
-    if (! is_dir($directory)) {
-        @mkdir($directory, 0775, true);
-    }
-}
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
